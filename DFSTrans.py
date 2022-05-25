7d52003da930a128f0c2df236c8c0e3059dfc42a
dfstrans <106074588+dfstrans@users.noreply.github.com>
2022-05-25 15:21:03 +0200

    Update DFSTrans.py

--- DFSTrans.py
+++ DFSTrans.py
@@ -20,7 +20,35 @@ import csv
 from openpyxl import load_workbook
 from sklearn.preprocessing import MinMaxScaler
 
+class Flatten(nn.Module):
 
+    def forward(self, x):
+        batch_size = x.shape[0]
+        return x.view(batch_size, -1)
+
+
+class TimeDistributed(nn.Module):
+    def __init__(self, module, batch_first=True):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+        self.batch_first = batch_first
+
+    def forward(self, x):
+
+        if len(x.size()) <= 2:
+            return self.module(x)
+
+        x_reshape = x.contiguous().view(-1, *(x.size()[2:]))
+        y = self.module(x_reshape)
+
+        # We have to reshape Y
+        if self.batch_first:
+            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
+        else:
+            y = y.contiguous().view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
+
+        return y
+    
 def _get_activation_fn(activation):
     if activation == "relu":
         return F.relu
