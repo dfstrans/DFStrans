a288eeb13cd6a24d5c448b2e4bf049b4e5d98efe
dfstrans <106074588+dfstrans@users.noreply.github.com>
2022-05-25 15:19:33 +0200

    Update DFSTrans.py

--- DFSTrans.py
+++ DFSTrans.py
@@ -182,9 +182,9 @@ class TransSensor(nn.Module):
 class DFSTrans_model(nn.Module):
 
     def __init__(self, activation="relu", d_model=240, dim_feedforward=2048,
-                 dropout=0.1,n_channels = 20,n_time_steps = 80,output_dim = 4800, n_units_l1 = 512):
+                 dropout=0.1,n_channels = 20,n_time_steps = 80,output_dim = 4800, n_units_l1 = 512, conv_filters = 20):
         super(DFSTrans_model, self).__init__()
-        self.conv_cell = nn.ModuleList([MultiHead1DCNN(time_steps=n_time_steps,n_channels=n_channels) for i in range(n_channels)])
+        self.conv_cell = nn.ModuleList([MultiHead1DCNN(conv_filters = conv_filters, time_steps = n_time_steps) for i in range(n_channels)])
         self.TimeDistributed_flatten = nn.ModuleList([TimeDistributed(Flatten) for i in range(n_channels)])
         self.trace = []
         self.TransformerTS_list = nn.ModuleList([TransTS() for i in range(n_channels)])
@@ -214,7 +214,7 @@ class DFSTrans_model(nn.Module):
     def forward(self, input_x):
 
         trace = []
-        for sensor_n in range(20):
+        for sensor_n in range(self.n_channels):
             input_layer = input_x[sensor_n]
             input_layer_reshape = input_layer.view(-1, *(input_layer.size()[2:]))
             x = self.conv_cell[sensor_n](input_layer_reshape)
